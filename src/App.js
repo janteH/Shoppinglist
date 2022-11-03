0a0974d90c7c85f8cc313068cf0f7a9e9179294f
Jante <jante.wallin@gmail.com>
2022-11-03 19:41:08 +0200

    tehtävä valmis

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState,useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost:8888/todo2022/';
 
 function App() {
+  const [item,setItem] = useState('');
+  const [items,setItems] = useState([]);
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setItems(response.data);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }, [])
+  
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({description:item});
+    axios.post(URL + 'add.php',json, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      setItems(items => [...items,response.data]);
+      setItem('');
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <form onSubmit={save}>
+        <label>New item</label>
+        <input value={item} placeholder='Add a new item' onChange={e => setItem(e.target.value)} />
+        <button>Save</button>
+      </form>
+      <ol>
+        {items?.map(item =>(
+          <li key={item.id}>{item.description}</li>
+        ))}
+      </ol>
     </div>
   );
 }
---
+++ src/add.php
@@ -0,0 +1,18 @@
+<?php
+require_once 'inc/headers.php';
+require_once 'inc/functions.php';
+
+$input = json_decode(file_get_contents('php://input'));
+$description = filter_var($input->description,FILTER_SANITIZE_SPECIAL_CHARS);
+
+try {
+  $db = openDB();
+  $query = $db->prepare('insert into task(description) values (:description)');
+  $query->bindValue(':description',$description,PDO::PARAM_STR);
+  $query->execute();
+  header('HTTP/1.1 200 OK');
+  $data = array('id' => $db->lastInsertId(),'description' => $description);
+  print json_encode($data);
+} catch (PDOException $pdoex) {
+  returnError($pdoex);
+}
